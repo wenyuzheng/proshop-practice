9339cd116c849c83ce17dfec96b1b8365ff9923c
Wenyu Zheng <zhengwy229@gmail.com>
2024-03-28 12:51:43 +0000

    Added redux

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,6 +4,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .products import products
+from .models import Product
+
+from .serializers import ProductSerializer
 
 # Create your views here.
 
@@ -14,15 +17,13 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
-
-    return Response(product)
+    product = Product.objects.get(_id=pk)
+    serializer = ProductSerializer(product, many=False)
+    return Response(serializer.data)
 

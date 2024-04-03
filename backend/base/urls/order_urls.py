9ae49b3b1364306e3f2ed21deb7c910793c185ac
Wenyu Zheng <zhengwy229@gmail.com>
2024-04-03 17:16:07 +0100

    Set up order details backend

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from base.views import order_views as views
 
 urlpatterns = [
-    path('add/', view=views.addOrderItems, name='orders-add')
+    path('add/', view=views.addOrderItems, name='orders-add'),
+    path('<str:pk>/', view=views.getOrderById, name='user-order'),
 ]

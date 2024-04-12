fd083f9f28a122cb23422fbbc64518c5841fe69c
Wenyu Zheng <zhengwy229@gmail.com>
2024-04-12 16:53:26 +0100

    OrderListScreen complete

--- backend/base/urls/order_urls.py
+++ backend/base/urls/order_urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from base.views import order_views as views
 
 urlpatterns = [
+    path('', view=views.getOrders, name='orders'),
     path('add/', view=views.addOrderItems, name='orders-add'),
     path('myorders/', view=views.getMyOrders, name='myorders'),
     path('<str:pk>/', view=views.getOrderById, name='user-order'),
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,6 +18,7 @@ import UserListScreen from "./screens/UserListScreen";
 import UserEditScreen from "./screens/UserEditScreen";
 import ProductListScreen from "./screens/ProductListScreen";
 import ProductEditScreen from "./screens/ProductEditScreen";
+import OrderListScreen from "./screens/OrderListScreen";
 
 function App() {
   return (
@@ -43,6 +44,7 @@ function App() {
               path="/admin/product/:id/edit"
               element={<ProductEditScreen />}
             />
+            <Route path="/admin/orderlist" element={<OrderListScreen />} />
           </Routes>
         </Container>
       </main>
